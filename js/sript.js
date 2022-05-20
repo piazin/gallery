1c33b9c3469cbbc27a589e2ce948ba2d62ff05a6
piazin <ls4803326@gmail.com>
2022-05-19 21:26:57 -0300

    update loading

--- js/sript.js
+++ js/sript.js
@@ -1,6 +1,20 @@
 function loading() {
+    window.scrollTo(0, 0);
+
     setTimeout(()=>{
         document.getElementById('loading').style.display = "none";
-        document.getElementById('body').style.overflowY = "scroll"
+        document.getElementById('body').style.overflowY = "scroll";
     }, 4000)
+}
+
+var audio = document.getElementById('audio');
+var isPlay = false;
+
+function playSong() {
+    if(audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        isPlay = false;
+    }
 }
